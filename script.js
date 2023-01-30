88c8bcfae94d4c01f35bd9aac57a546555640c25
OmobolajiKoyi <koyi@ais-security.de>
2023-01-30 13:19:39 +0100

    update cv

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
+let theme = localStorage.getItem("theme");
 
-
-let theme = localStorage.getItem('theme')
-
-if (theme==null){
-    setTheme('light')
-}else{
-    setTheme(theme)
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
 }
 
-let themeDots = document.getElementsByClassName('theme-dot')
+let themeDots = document.getElementsByClassName("theme-dot");
 
-for (let i=0; i<themeDots.length; i++){
-    console.log(themeDots.length);
-    themeDots[i].addEventListener('click', function(){
-        let mode=this.dataset.mode
-        console.log('option clicked:', mode)
-        setTheme(mode)
-    })
+for (let i = 0; i < themeDots.length; i++) {
+  console.log(themeDots.length);
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("option clicked:", mode);
+    setTheme(mode);
+  });
 }
 
 // hearts = document.querySelectorAll('.heart');
@@ -25,20 +23,20 @@ for (let i=0; i<themeDots.length; i++){
 //         await heart.classList.toggle('heartColor')
 //     }, 1000)
 // }
-document.querySelector('.year').textContent = new Date().getFullYear()
+document.querySelector(".year").textContent = new Date().getFullYear();
 
-function setTheme(mode){
-    if(mode==='light'){
-        document.getElementById('theme-style').href="default.css"
-    }
+function setTheme(mode) {
+  if (mode === "light") {
+    document.getElementById("theme-style").href = "default.css";
+  }
 
-    if(mode==='blue'){
-        document.getElementById('theme-style').href="blue-mode.css"
-    }
+  if (mode === "blue") {
+    document.getElementById("theme-style").href = "blue-mode.css";
+  }
 
-    if(mode==='green'){
-        document.getElementById('theme-style').href="green-mode.css"
-    }
+  if (mode === "green") {
+    document.getElementById("theme-style").href = "green-mode.css";
+  }
 
-    localStorage.setItem('theme', mode)
+  localStorage.setItem("theme", mode);
 }
